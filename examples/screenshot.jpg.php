b26448b32615f1cdda2d103e43e1ad2331094096
Pitos <contact@gawsoft.com>
2025-11-15 00:50:07 +0100

    Add WEBSHOTAPI_KEY env Fix Readme Add Video endpoint

--- examples/screenshot.jpg.php
+++ examples/screenshot.jpg.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . '/../vendor/autoload.php';
+include_once 'vendor/autoload.php';
 
 use Webshotapi\Client\Exceptions\WebshotApiClientException;
 use Webshotapi\Client\WebshotApiClient;
